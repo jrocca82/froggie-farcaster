eb3b1ac04116ff9e6b082ed4cb21c85262d23603
Jo <loluvulol@gmail.com>
2024-03-04 13:45:54 +1000

    fix: build

--- froggie/api/index.tsx
+++ froggie/api/index.tsx
@@ -1,65 +1,37 @@
-import { Button, Frog, TextInput } from "frog";
-import { handle } from "frog/vercel";
-
-export const app = new Frog({
-	assetsPath: "/",
-	basePath: "/api",
-	// Supply a Hub API URL to enable frame verification.
-	hubApiUrl: "https://api.hub.wevm.dev",
-});
-
-app.frame("/", (c) => {
-	const { buttonValue, inputText, status } = c;
-	const chain = inputText || buttonValue;
-	return c.res({
-		action: "/submit",
-		image: (
-			<div
-				style={{
-					alignItems: "center",
-					background:
-						status === "response"
-							? "linear-gradient(to right, #432889, #17101F)"
-							: "black",
-					backgroundSize: "100% 100%",
-					display: "flex",
-					flexDirection: "column",
-					flexWrap: "nowrap",
-					height: "100%",
-					justifyContent: "center",
-					textAlign: "center",
-					width: "100%",
-				}}
-			>
-				<div
-					style={{
-						color: "white",
-						fontSize: 60,
-						fontStyle: "normal",
-						letterSpacing: "-0.025em",
-						lineHeight: 1.4,
-						marginTop: 30,
-						display: "flex",
-						padding: "0 120px",
-						whiteSpace: "pre-wrap",
-					}}
-				>
-					Hello! Choose your favorite network
-					{status === "response"
-						? `Nice choice. ${chain ? ` ${chain.toUpperCase()}!!` : ""}`
-						: "Welcome!"}
-				</div>
-			</div>
-		),
-		intents: [
-			<TextInput placeholder="Enter custom chain..." />,
-			<Button value="mainnet">Mainnet</Button>,
-			<Button value="polygon">Polygon</Button>,
-			<Button value="base">Base</Button>,
-			status === "response" && <Button.Reset>Reset</Button.Reset>,
-		],
-	});
-});
-
-export const GET = handle(app);
-export const POST = handle(app);
+import { Button, Frog } from 'frog'
+import { handle } from 'frog/vercel' 
+ 
+// Uncomment to use Edge Runtime.
+// export const config = {
+//   runtime: 'edge',
+// }
+ 
+export const app = new Frog({ 
+  basePath: '/api',
+  browserLocation: '/:path',
+  // Supply a Hub API URL to enable frame verification.
+  hubApiUrl: 'https://api.hub.wevm.dev',
+})
+ 
+app.frame('/', (c) => {
+  const { buttonValue, status } = c
+  return c.res({
+    image: (
+      <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+        {status === 'initial' ? (
+          'Select your fruit!'
+        ) : (
+          `Selected: ${buttonValue}`
+        )}
+      </div>
+    ),
+    intents: [
+      <Button value="apple">Apple</Button>,
+      <Button value="banana">Banana</Button>,
+      <Button value="mango">Mango</Button>
+    ]
+  })
+})
+ 
+export const GET = handle(app) 
+export const POST = handle(app) 
